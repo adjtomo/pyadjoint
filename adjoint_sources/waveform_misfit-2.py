cd4901e98bbd027cbbbfaa49de2684c1d1f97f98
Travis <travis@travis-ci.org>
2015-02-05 23:27:24 +0000

    Travis build 8 pushed to gh-pages

---
+++ adjoint_sources/waveform_misfit-2.py
@@ -0,0 +1,10 @@
+import pyadjoint
+import matplotlib.pylab as plt
+fig = plt.figure(figsize=(12, 6))
+obs, syn = pyadjoint.utils.get_example_data()
+obs = obs.select(component="T")[0]
+syn = syn.select(component="T")[0]
+start, end = pyadjoint.utils.EXAMPLE_DATA_SDIFF
+pyadjoint.calculate_adjoint_source("waveform_misfit", obs, syn, 20.0, 100.0,
+                                   start, end, adjoint_src=True, plot=fig)
+plt.show()
